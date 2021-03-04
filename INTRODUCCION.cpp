63f6ddb41b4e9bd5658e59a5045aaba52dc6ecc5
Orft40 <79733636+Orft40@users.noreply.github.com>
2021-03-03 21:49:45 -0600

    Add files via upload

---
+++ INTRODUCCION.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int suma (int n1, int n2)
+{
+    return n1+n2;
+}
+main(){
+    int n1, n2;
+cout<<"introduzca el valor del primer operando";
+cin>> n1;
+cout<<"introduzca el valor del segundo operando";
+cin>> n2;
+cout<<suma(n1,n2);
+}
